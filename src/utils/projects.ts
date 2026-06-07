0b9478156d60e17188a7b8c34398783210e22595
nguyen566 <randy.nguyen@live.com>
2026-06-07 14:47:19 -0500

    added new project

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,120 +1,199 @@
 export const projectData = [
-	{
-		key: 1,
-		name: "Reddit Clone Site",
-		type: "Fullstack Development",
-		skills: [
-			{
-				skillKey: 1,
-				skillDesc: "ReactJS",
-			},
-			{
-				skillKey: 2,
-				skillDesc: "NextJS",
-			},
-			{
-				skillKey: 3,
-				skillDesc: "TypeScript",
-			},
-			{
-				skillKey: 4,
-				skillDesc: "GraphQL",
-			},
-			{
-				skillKey: 5,
-				skillDesc: "URQL/Apollo",
-			},
-			{
-				skillKey: 6,
-				skillDesc: "NodeJS",
-			},
-			{
-				skillKey: 7,
-				skillDesc: "PostgresSQL",
-			},
-			{
-				skillKey: 8,
-				skillDesc: "TypeORM",
-			},
-			{
-				skillKey: 9,
-				skillDesc: "Redis",
-			},
-			{
-				skillKey: 10,
-				skillDesc: "TypeGraphQL",
-			},
-			{
-				skillKey: 11,
-				skillDesc: "Chakra",
-			},
-		],
-		github: "https://github.com/nguyen566/fullstack-graphql-reddit-clone",
-		deployed: undefined,
-		description: [
-			{
-				descKey: 1,
-				desc: "Developed a clone of reddit website by implementing fullstack knowledge of NextJS for frontend, GraphQL for backend, and PostgresSQL for database",
-			},
-			{
-				descKey: 2,
-				desc: "Implemented JWT Token access to enable users to reset password if they forgot it",
-			},
-			{
-				descKey: 3,
-				desc: "Created entities and resolvers using TypeORM and allowed mutations and queries using GraphQL",
-			},
-			{
-				descKey: 4,
-				desc: "Connected app to Redis using Ubuntu to allow storage of cookies and caching",
-			},
-			{
-				descKey: 5,
-				desc: "Styled application using Chakra UI",
-			},
-		],
-	},
-	{
-		key: 2,
-		name: "Personal Portfolio",
-		type: "Frontend Development",
-		skills: [
-			{
-				skillKey: 1,
-				skillDesc: "ReactJS",
-			},
-			{
-				skillKey: 2,
-				skillDesc: "NextJS",
-			},
-			{
-				skillKey: 3,
-				skillDesc: "TypeScript",
-			},
-			{
-				skillKey: 4,
-				skillDesc: "NodeJS",
-			},
-			{
-				skillKey: 5,
-				skillDesc: "Chakra",
-			},
-		],
-		github: "https://github.com/nguyen566/nguyen566.github.io",
-		deployed: "https://randy-nguyen-portfolio.vercel.app/",
-		description: [
-			{
-				descKey: 1,
-				desc: "Designed personal portfolio website to demonstrate knowledge in NextJS framework",
-			},
-			{
-				descKey: 2,
-				desc: "NextJS framework was coded in TypeScript and styled with Chakra UI",
-			},
-			{
-				descKey: 3,
-				desc: "Deployed using Vercel",
-			},
-		],
-	},
+  {
+    key: 1,
+    name: "Job Tracker Dashboard",
+    type: "Fullstack Development",
+    skills: [
+      {
+        skillKey: 1,
+        skillDesc: "Python",
+      },
+      {
+        skillKey: 2,
+        skillDesc: "FastAPI",
+      },
+      {
+        skillKey: 3,
+        skillDesc: "Redis",
+      },
+      {
+        skillKey: 4,
+        skillDesc: "JWT",
+      },
+      {
+        skillKey: 5,
+        skillDesc: "OAuth2",
+      },
+      {
+        skillKey: 6,
+        skillDesc: "REST Api",
+      },
+      {
+        skillKey: 7,
+        skillDesc: "PostgresSQL",
+      },
+      {
+        skillKey: 8,
+        skillDesc: "SQL Alchemy + Model",
+      },
+      {
+        skillKey: 9,
+        skillDesc: "ReactJS",
+      },
+      {
+        skillKey: 10,
+        skillDesc: "Typescript",
+      },
+      {
+        skillKey: 11,
+        skillDesc: "ShadCN-UI",
+      },
+      {
+        skillKey: 12,
+        skillDesc: "Docker compose ready",
+      },
+      {
+        skillKey: 13,
+        skillDesc: "pwdhash",
+      },
+    ],
+    github: "https://github.com/nguyen566/python-react-fullstack",
+    deployed: undefined,
+    description: [
+      {
+        descKey: 1,
+        desc: "Job tracking site used to keep maintain a list of applied jobs for personal use",
+      },
+      {
+        descKey: 2,
+        desc: "Users can go through typical user creation process, login, and reset password process using JWT and OAuth2",
+      },
+      {
+        descKey: 3,
+        desc: "Created a docker-compose.yaml for easy set up to use app",
+      },
+      {
+        descKey: 4,
+        desc: "Ability to submit job application through form and modify application status",
+      },
+    ],
+  },
+  {
+    key: 2,
+    name: "Reddit Clone Site",
+    type: "Fullstack Development",
+    skills: [
+      {
+        skillKey: 1,
+        skillDesc: "ReactJS",
+      },
+      {
+        skillKey: 2,
+        skillDesc: "NextJS",
+      },
+      {
+        skillKey: 3,
+        skillDesc: "TypeScript",
+      },
+      {
+        skillKey: 4,
+        skillDesc: "GraphQL",
+      },
+      {
+        skillKey: 5,
+        skillDesc: "URQL/Apollo",
+      },
+      {
+        skillKey: 6,
+        skillDesc: "NodeJS",
+      },
+      {
+        skillKey: 7,
+        skillDesc: "PostgresSQL",
+      },
+      {
+        skillKey: 8,
+        skillDesc: "TypeORM",
+      },
+      {
+        skillKey: 9,
+        skillDesc: "Redis",
+      },
+      {
+        skillKey: 10,
+        skillDesc: "TypeGraphQL",
+      },
+      {
+        skillKey: 11,
+        skillDesc: "Chakra",
+      },
+    ],
+    github: "https://github.com/nguyen566/fullstack-graphql-reddit-clone",
+    deployed: undefined,
+    description: [
+      {
+        descKey: 1,
+        desc: "Developed a clone of reddit website by implementing fullstack knowledge of NextJS for frontend, GraphQL for backend, and PostgresSQL for database",
+      },
+      {
+        descKey: 2,
+        desc: "Implemented JWT Token access to enable users to reset password if they forgot it",
+      },
+      {
+        descKey: 3,
+        desc: "Created entities and resolvers using TypeORM and allowed mutations and queries using GraphQL",
+      },
+      {
+        descKey: 4,
+        desc: "Connected app to Redis using Ubuntu to allow storage of cookies and caching",
+      },
+      {
+        descKey: 5,
+        desc: "Styled application using Chakra UI",
+      },
+    ],
+  },
+  {
+    key: 3,
+    name: "Personal Portfolio",
+    type: "Frontend Development",
+    skills: [
+      {
+        skillKey: 1,
+        skillDesc: "ReactJS",
+      },
+      {
+        skillKey: 2,
+        skillDesc: "NextJS",
+      },
+      {
+        skillKey: 3,
+        skillDesc: "TypeScript",
+      },
+      {
+        skillKey: 4,
+        skillDesc: "NodeJS",
+      },
+      {
+        skillKey: 5,
+        skillDesc: "Chakra",
+      },
+    ],
+    github: "https://github.com/nguyen566/nguyen566.github.io",
+    deployed: "https://randy-nguyen-portfolio.vercel.app/",
+    description: [
+      {
+        descKey: 1,
+        desc: "Designed personal portfolio website to demonstrate knowledge in NextJS framework",
+      },
+      {
+        descKey: 2,
+        desc: "NextJS framework was coded in TypeScript and styled with Chakra UI",
+      },
+      {
+        descKey: 3,
+        desc: "Deployed using Vercel",
+      },
+    ],
+  },
 ];
