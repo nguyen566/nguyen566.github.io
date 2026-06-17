8f5cdce448e6a26372d269691ec06cc08b46e246
nguyen566 <randy.nguyen@live.com>
2026-06-17 13:25:23 -0500

    added conditional rendering for icon button

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,71 +1,77 @@
 import {
-	Box,
-	Flex,
-	Heading,
-	IconButton,
-	ListItem,
-	Text,
-	UnorderedList,
+  Box,
+  Flex,
+  Heading,
+  IconButton,
+  ListItem,
+  Text,
+  UnorderedList,
 } from "@chakra-ui/react";
-import { FaGithub, FaExternalLinkAlt, } from "react-icons/fa";
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 import { projectData } from "../utils/projects";
 
 const Project = () => {
-	return (
-		<>
-			<Heading size={"2xl"} mt={5} mb={6}>
-				Projects
-			</Heading>
-			<Flex flexDir="column" mb={5}>
-				{projectData.map((project) => {
-					return (
-						<Box key={project.key}>
-							<Heading size={"lg"} mb={3}>
-								{project.name}
-							</Heading>
-							<Text as="i" mb={3}>
-								{project.type}
-							</Text>
-							<Flex justifyContent={"start"} mb={3} flexDir={['column', 'column', 'column', 'row']}>
-								{project.skills.map((skill) => {
-									return (
-										<Text key={skill.skillKey} mr={3}>
-											{skill.skillDesc}
-										</Text>
-									);
-								})}
-							</Flex>
-							<UnorderedList>
-								{project.description.map((desc) => {
-									return <ListItem key={desc.descKey}>{desc.desc}</ListItem>;
-								})}
-							</UnorderedList>
-							<Flex my={5} alignItems="center" gap="1rem">
-								<IconButton
-									as="a"
-									href={project.github}
-									target="_blank"
-									rel="noopener noreferrer"
-									aria-label="Project github"
-									icon={<FaGithub size={30} />}
-								/>
-								{project.deployed !== undefined ? (
-									<IconButton
-										as="a"
-										href={project.deployed}
-										target="_blank"
-										rel="noopener noreferrer"
-										aria-label="Deployed project"
-										icon={<FaExternalLinkAlt size={30} />}
-									/>
-								) : null}
-							</Flex>
-						</Box>
-					);
-				})}
-			</Flex>
-		</>
-	);
+  return (
+    <>
+      <Heading size={"2xl"} mt={5} mb={6}>
+        Projects
+      </Heading>
+      <Flex flexDir="column" mb={5}>
+        {projectData.map((project) => {
+          return (
+            <Box key={project.key}>
+              <Heading size={"lg"} mb={3}>
+                {project.name}
+              </Heading>
+              <Text as="i" mb={3}>
+                {project.type}
+              </Text>
+              <Flex
+                justifyContent={"start"}
+                mb={3}
+                flexDir={["column", "column", "column", "row"]}
+              >
+                {project.skills.map((skill) => {
+                  return (
+                    <Text key={skill.skillKey} mr={3}>
+                      {skill.skillDesc}
+                    </Text>
+                  );
+                })}
+              </Flex>
+              <UnorderedList>
+                {project.description.map((desc) => {
+                  return <ListItem key={desc.descKey}>{desc.desc}</ListItem>;
+                })}
+              </UnorderedList>
+              <Flex my={5} alignItems="center" gap="1rem">
+                {project.github !== undefined ? (
+                  <IconButton
+                    as="a"
+                    href={project.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="Project github"
+                    icon={<FaGithub size={30} />}
+                  />
+                ) : null}
+                {project.deployed !== undefined ? (
+                  <IconButton
+                    as="a"
+                    href={project.deployed}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="Deployed project"
+                    icon={<FaExternalLinkAlt size={30} />}
+                  />
+                ) : null}
+              </Flex>
+            </Box>
+          );
+        })}
+      </Flex>
+    </>
+  );
 };
 
 export default Project;
